c916d5c13ab286a96051d1fabc9d1926e0d4bfb8
Kryukova Evgeniya <Evgenijavk@mail.ru>
2020-05-16 10:47:51 +0900

    mogo

---
+++ assets/js/app.js
@@ -0,0 +1,68 @@
+$(function () {
+  var header = $("#header"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  // FIXED HEADER
+
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  // SMOOTH SCROLL
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+  });
+
+  /* MENU NAV TOGGLE */
+  $("#nav_toggle").on("click", function (event) {
+    event.preventDefault();
+
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+  });
+
+  /* COLLAPSE */
+  $("[data-collapse]").on("click", function (event) {
+    event.preventDefault();
+
+    var $this = $(this),
+      blockId = $this.data("collapse");
+
+    $this.toggleClass("active");
+  });
+
+  /* SLIDER */
+  $("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
